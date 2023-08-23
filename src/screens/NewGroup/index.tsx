87b8868b4e5b229ca37e74bc6aad8316c5573369
AndersonRR <anderson.ribeiro.r@outlook.com>
2023-08-23 11:06:56 -0300

    feat: adiciona componente de input

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,7 +1,9 @@
 import { Container, Content, Icon } from './styles';
+
 import { Highlight } from '@components/Highlight';
 import { Header } from '@components/Header';
 import { Button } from '@components/Button';
+import { Input } from '@components/Input';
 
 export function NewGroup() {
   return (
@@ -10,12 +12,13 @@ export function NewGroup() {
       <Content>
         <Icon />
         <Highlight
-          title="Nova turma"
-          subtitle="crie a turma para adicionar as pessoas"
+          title='Nova turma'
+          subtitle='crie a turma para adicionar as pessoas'
         />
-
+        <Input placeholder='Nome da turma' />
         <Button
-          title="Criar"
+          title='Criar'
+          style={{ marginTop: 24 }}
         />
       </Content>
     </Container>
